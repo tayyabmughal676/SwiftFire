79203bc4ed95072d0a5a92b27b4d8fd1be531649
Mr. Tayyab Mughal <tayyabmughal676@gmail.com>
2023-04-24 22:03:34 +0500

    Initial Commit

---
+++ SwiftFire/SwiftFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFireApp.swift
+//  SwiftFire
+//
+//  Created by Thor on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
